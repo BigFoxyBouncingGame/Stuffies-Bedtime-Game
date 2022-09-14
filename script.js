4d25f398da966b558280260800cb3d57e59eeed5
BigFoxyBouncingGame <easton.r2020@gmail.com>
2022-09-13 18:20:23 -0700

    Fix animations not playing, remove unneeded code.

--- script.js
+++ script.js
@@ -395,14 +395,6 @@ function setupGame() {
             level_meshes = result.meshes;
             var room_meshes = [];
             for (var i = 0; i < result.meshes.length; i++) {
-                if (level_meshes[i].material != null) {
-                    if (level_meshes[i].material.id == "Glass" || level_meshes[i].material.id == "Window") {
-                        level_meshes[i].material.alpha = 0.1;
-                    }
-                    if (level_meshes[i].material.id == "Water") {
-                        level_meshes[i].material.alpha = 0.8;
-                    }
-                }
                 level_meshes[i].receiveShadows = true;
                 if (level_meshes[i].id.startsWith("Room")) {
                     room_meshes.push(level_meshes[i]);
@@ -433,6 +425,9 @@ function setupGame() {
                         foxyPitchAnimation = scene.animationGroups[i];
                         foxyPitchAnimation.goToFrame(30);
                         foxyPitchAnimation.stop();
+                    }
+                    else {
+                        scene.animationGroups[i].play();
                     }
                 }
 
